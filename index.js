703e8f1302e4a075b11e639e8557fcfa1482f1ff
CarmenJvR <50364617+CarmenJvR@users.noreply.github.com>
2021-05-12 09:18:56 +0200

    heoku postress setup

--- index.js
+++ index.js
@@ -9,11 +9,30 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 const port = process.env.PORT || 3000;
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
-
-app.get('/', (req, res) => {
+/* app.get('/', (req, res) => {
     res.send("Simple API Gateway")
-})
+}) */
+
+app.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM account');
+      const results = { 'results': (result) ? result.rows : null};
+      res.send(JSON.stringify(results));
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
 
 app.use(router)
 
