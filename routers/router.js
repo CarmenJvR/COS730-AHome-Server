262644cd22c4ea9c344df6a83281f434095e366e
CarmenJvR <50364617+CarmenJvR@users.noreply.github.com>
2021-05-12 12:36:44 +0200

    project service initialized

--- routers/router.js
+++ routers/router.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router()
 var accountService = require('./accountService')
+var projectService = require('./projectService')
 
 router.use((req, res, next) => {
     console.log("Called: ", req.path)
@@ -8,5 +9,6 @@ router.use((req, res, next) => {
 })
 
 router.use(accountService)
+router.use(projectService)
 
 module.exports = router
